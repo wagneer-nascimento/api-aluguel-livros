e0a39f14b87af815e54280687dd7e06d3a01e3d7
wagneernasc <wagneer_nascimento@live.com>
2021-02-14 19:53:57 -0300

    excptions e outras classes

---
+++ src/main/java/desafio/digivox/DigivoxApplication.java
@@ -0,0 +1,15 @@
+package desafio.digivox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigivoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigivoxApplication.class, args);
+		System.out.println("Staaaaarter !! :) ");
+
+	}
+
+}
---
+++ src/test/java/desafio/digivox/DigivoxApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.digivox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigivoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
